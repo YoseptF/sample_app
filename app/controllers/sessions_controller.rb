a1cfec10752b746b211ecf25362e9f3583f51c8c
Yosept <yosept.flores@gmail.com>
2020-02-26 09:02:08 -0600

    I created the base files to send mails

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
+    @example = 1
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test 'should get new' do
+    p assigns(:example)
     get login_path
     assert_response :success
   end
